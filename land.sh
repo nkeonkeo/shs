936db1d6fdb14e93f65715e0d8432824b971e062
neko <36293036+nkeonkeo@users.noreply.github.com>
2022-01-16 12:08:47 +0800

    Update land.sh

--- land.sh
+++ land.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+os=`uname -s | tr [:upper:] [:lower:]`
+arch=`uname -m`
 
-wget https://github.com/nkeonkeo/neko-relay-land/releases/latest/download/neko-relay_linux_amd64 -O /usr/bin/neko-relay || curl https://github.com/nkeonkeo/neko-relay-land/releases/latest/download/neko-relay_linux_amd64 -o /usr/bin/neko-relay
+case ${arch} in
+x86)
+arch="386"
+;;
+x86_64)
+arch="amd64"
+;;
+aarch64)
+arch="arm64"
+;;
+esac
+url="https://github.com/nkeonkeo/neko-relay-land/releases/latest/download/neko-relay_"${os}"_"${arch}
+wget ${url} -O /usr/bin/neko-relay || curl https://github.com/nkeonkeo/neko-relay-land/releases/latest/download/neko-relay_linux_amd64 -o /usr/bin/neko-relay
 chmod +x /usr/bin/neko-relay
 neko-relay -v && echo "neko-relay自定义隧道落地端安装成功！"
 if [ ! -d "/etc/neko-relay" ]; then
